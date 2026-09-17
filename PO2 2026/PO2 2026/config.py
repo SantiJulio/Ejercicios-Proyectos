d7d83ddd3fe5b8a4bdae04d563bb84f5684f62dd
Santiago <110004667+SantiJulio@users.noreply.github.com>
2026-09-16 21:01:32 -0300

    Delete PO2 2026/PO2 2026 directory

--- PO2 2026/PO2 2026/config.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY = "POO-ES-LA-MEJOR" #se usa en ciertas operaciones que requieren seguridad
-SQLALCHEMY_DATABASE_URI = 'sqlite:///datos.sqlite3' #indica el nombre del archivo que contiene la base de datos
-SQLACHMEMY_TRACK_MODIFICATIONS = False # esta carateristica rastrea las modificaciones de los objetos y emite señales, debe dejase en False debido a q requiere memoria adicional
